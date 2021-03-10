ed2929ad3ff0f921a225e11e8ebec7b15ac91116
Johann Walendzik <Johann Walendzik@DESKTOP-4CIIT3K.fritz.box>
2021-03-10 15:44:34 +0100

    Third try

--- recommendationObj/src/recommendation/house.java
+++ recommendationObj/src/recommendation/house.java
@@ -21,6 +21,8 @@ public class house{
 	//test comment johann
 	//test comment eva
 	
+	//third test comment
+	
 	public house(boolean hasGarden, int noOfRooms, int noOfBathrooms, int floors, double price, double livingArea, 
 				 String address, String energyLabel, int houseId, int sellerId, int acceptedBid) {
 		this.hasGarden = hasGarden;
